684f3e20459166686c8396eda1f7ae6fd6939241
tonyfwu <tonyfwu@eac4872d-9840-0410-83f7-a96378a11634>
2008-02-29 10:16:16 +0000

    Added Saturation filter.

--- trunk/java/WebcamJava_win32/src/webcamjava/Main.java
+++ trunk/java/WebcamJava_win32/src/webcamjava/Main.java
@@ -25,7 +25,12 @@ public class Main {
 		Webcam cam = new Webcam(FrameGrabber.DEFAULT_DEV_NAME);
 		CamFrame frame = new CamFrame(cam);
 		CamFrame orgFrame = new CamFrame(cam);
-		frame.setFilter(new RGBFilter(150, 56, 42, 50));
+		CamFilter[] filters = new CamFilter[2];
+		filters[0] = new RGBFilter(150, 56, 42, 50);
+		filters[1] = new SatFilter(0.6f, 1.0f);
+		frame.setFilter(new TestFilter(filters));;
+		//frame.setFilter(new SatFilter(0.5f, 1.0f));
+		//frame.setFilter(new RGBFilter(150, 56, 42, 50));
 		frame.setVisible(true);
 		orgFrame.setVisible(true);
 		Thread frameThread = new Thread(frame);
@@ -35,14 +40,14 @@ public class Main {
 		String s;
 		int r, g, b, t;
 		while (true){
-			System.out.print("Set RGB and Threshold: ");
+			System.out.print("Set RGB and Threshold (R G B T): ");
 			s = scan.nextLine();
 			st = new StringTokenizer(s, " ");
 			r = Integer.parseInt(st.nextToken());
 			g = Integer.parseInt(st.nextToken());
 			b = Integer.parseInt(st.nextToken());
 			t = Integer.parseInt(st.nextToken());
-			frame.setFilter(new RGBFilter(r, g, b, t));
+			filters[0] = new RGBFilter(r, g, b, t);
 		}
 	    } catch (FrameGrabberException fge){
 		fge.printStackTrace();
