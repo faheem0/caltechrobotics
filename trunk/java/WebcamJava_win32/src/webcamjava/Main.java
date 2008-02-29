94b597cc3acd201ddb8aca0990fae1d9f430dd93
tonyfwu <tonyfwu@eac4872d-9840-0410-83f7-a96378a11634>
2008-02-29 08:50:34 +0000

    Multithreaded!

--- trunk/java/WebcamJava_win32/src/webcamjava/Main.java
+++ trunk/java/WebcamJava_win32/src/webcamjava/Main.java
@@ -7,6 +7,8 @@ package webcamjava;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 /**
  *
  * @author tonyfwu
@@ -17,7 +19,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+	    Scanner scan = new Scanner(System.in);
+	    StringTokenizer st;
 	    try{
 		Webcam cam = new Webcam(FrameGrabber.DEFAULT_DEV_NAME);
 		CamFrame frame = new CamFrame(cam);
@@ -25,16 +28,21 @@ public class Main {
 		frame.setFilter(new RGBFilter(150, 56, 42, 50));
 		frame.setVisible(true);
 		orgFrame.setVisible(true);
+		Thread frameThread = new Thread(frame);
+		Thread orgThread = new Thread(orgFrame);
+		frameThread.start();
+		orgThread.start();
+		String s;
+		int r, g, b, t;
 		while (true){
-			frame.updateImage();
-			frame.repaint();
-			orgFrame.updateImage();
-			orgFrame.repaint();
-			try {
-				Thread.sleep(100);
-			} catch(Exception e) {
-				System.out.println("Doh");
-			}
+			System.out.print("Set RGB and Threshold: ");
+			s = scan.nextLine();
+			st = new StringTokenizer(s, " ");
+			r = Integer.parseInt(st.nextToken());
+			g = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			t = Integer.parseInt(st.nextToken());
+			frame.setFilter(new RGBFilter(r, g, b, t));
 		}
 	    } catch (FrameGrabberException fge){
 		fge.printStackTrace();
