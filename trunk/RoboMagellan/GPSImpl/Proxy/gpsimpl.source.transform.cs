29ac282d6cba3d538878f291387d284bf28f1a2f
ksarma <ksarma@eac4872d-9840-0410-83f7-a96378a11634>
2008-03-31 18:20:37 +0000

    added robomagellan stuff

---
+++ trunk/RoboMagellan/GPSImpl/Proxy/gpsimpl.source.transform.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Dss.Core.Attributes;
+using Microsoft.Dss.Core.Transforms;
+
+#if NET_CF20
+[assembly: ServiceDeclaration(DssServiceDeclaration.Transform, SourceAssemblyKey = @"cf.gpsimpl.y2008.m03, version=0.0.0.0, culture=neutral, publickeytoken=00557257f0c3f8b7")]
+#else
+[assembly: ServiceDeclaration(DssServiceDeclaration.Transform, SourceAssemblyKey = @"gpsimpl.y2008.m03, version=0.0.0.0, culture=neutral, publickeytoken=00557257f0c3f8b7")]
+#endif
+#if !URT_MINCLR
+[assembly: System.Security.SecurityTransparent]
+[assembly: System.Security.AllowPartiallyTrustedCallers]
+#endif
+
+namespace Dss.Transforms.TransformGPSImpl
+{
+
+    public class Transforms: TransformBase
+    {
+        static Transforms()
+        {
+            Register();
+        }
+        public static void Register()
+        {
+        }
+    }
+}
+
---
+++ trunk/RoboMagellan/GenericIMU/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IMU
+{
+   class Program
+   {
+       static void Main(string[] args)
+       {
+           CristaIMU imu = new CristaIMU("COM2");
+           imu.autoupdate();
+       }
+   }
+}
