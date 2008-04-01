310c71bbc601d9a860d39ec1b853e7013f6d6bc6
tonyfwu <tonyfwu@eac4872d-9840-0410-83f7-a96378a11634>
2008-04-01 02:48:15 +0000

    Service should work now. There are some issues with empty strings. It could be a GPS hardware problem.

---
+++ AC12GPS.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+using Microsoft.Ccr.Core;
+using Microsoft.Dss.Core;
+using Microsoft.Dss.Core.Attributes;
+using Microsoft.Dss.ServiceModel.Dssp;
+using Microsoft.Dss.Core.DsspHttp;
+using Microsoft.Dss.ServiceModel.DsspServiceBase;
+
+using RoboMagellan.GenericGPS;
+
+namespace RoboMagellan.GenericGPS
+{
+
+    public class GpsDataPort : PortSet<UTMData, Exception> { }
+
+    public class AC12GPS
+    {
+        static int DEFAULT_BAUD_RATE = 4800;
+        static int DEFAULT_DATA_BITS = 8;
+        static int DEFAULT_TIMEOUT = 500;
+        static Parity DEFAULT_PARITY = Parity.None;
+        static StopBits DEFAULT_STOP_BITS = StopBits.One;
+        static Handshake DEFAULT_HANDSHAKE = Handshake.None;
+
+        //static Regex GGA = new Regex(@"^\$GPGGA");
+        //static Regex GSV = new Regex(@"^\$GPGSV");
+        //static Regex GSA = new Regex(@"^\$GPGSA");
+        //static Regex VTG = new Regex(@"^\$GPVTG");
+        private static Regex UTM = new Regex(@"^\$PASHR,UTM");
+
+        private volatile SerialPort myGPS;
+
+        private GpsDataPort gpsDataPort;
+
+
+        public AC12GPS(string portName, GpsDataPort dataPort)
+        {
+            myGPS = new SerialPort();
+            gpsDataPort = dataPort;
+
+            //Initialize Serial Port Parameters
+            myGPS.PortName = portName;
+            myGPS.BaudRate = DEFAULT_BAUD_RATE;
+            myGPS.Parity = DEFAULT_PARITY;
+            myGPS.DataBits = DEFAULT_DATA_BITS;
+            myGPS.StopBits = DEFAULT_STOP_BITS;
+            myGPS.Handshake = DEFAULT_HANDSHAKE;
+
+            myGPS.ReadTimeout = DEFAULT_TIMEOUT;
+            myGPS.WriteTimeout = DEFAULT_TIMEOUT;
+
+
+        }
+
+        public bool initializePort()
+        {
+
+            myGPS.Open();
+
+
+            if (myGPS.IsOpen)
+            {
+                Console.WriteLine("Sucessfully Connected!");
+                Console.WriteLine(myGPS.ToString());
+
+                myGPS.RtsEnable = true;
+
+                return true;
+            }
+
+            else
+            {
+                Console.WriteLine("Connection Failed!");    //Oh Shit!
+                return false;
+            }
+        }
+
+        public void activateHandler()
+        {
+            myGPS.DataReceived += new SerialDataReceivedEventHandler(serialPort_dataRecieved);
+        }
+
+
+
+        /**
+         * Converts a command string into ASCII and send the command
+         */
+        public void command(string s)
+        {
+            s = s + "\r\n";
+            byte[] asciiString = System.Text.Encoding.ASCII.GetBytes(s.ToCharArray());
+            myGPS.Write(asciiString, 0, asciiString.Length);
+        }
+
+        private void serialPort_dataRecieved(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                if (e.EventType == SerialData.Chars)
+                {
+                    string bufferString = myGPS.ReadLine();
+                    UTMData d = parseString(bufferString);
+                    gpsDataPort.Post(d);
+                }
+            }
+            catch (GPSDataParseException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        class GPSDataParseException : Exception {
+
+            public GPSDataParseException() : base() { }
+
+            public GPSDataParseException(string explanation) : base(explanation) { }
+
+            public GPSDataParseException(string exp, Exception ex) : base(exp, ex) { }
+        }
+
+        private UTMData parseString(string s)
+        {
+            Match m;
+            m = UTM.Match(s);   //MMmm.. Regular Expressions
+            UTMData d = new UTMData();
+            if (m.Success)
+            {
+                String[] utm_data = s.Split(',');
+                try
+                {
+                    //Parse the needed parameters
+                    double east = double.Parse(utm_data[4]);
+                    double north = double.Parse(utm_data[5]);
+                    int sat = int.Parse(utm_data[7]);
+                    double time = double.Parse(utm_data[2]);
+
+                    //Set the new data only if the above lines did not generate FormatException
+                        //Monitor.Enter(myData);
+                    
+                    d.East = east;
+                    d.North = north;
+                    d.NumSat = sat;
+                    d.Timestamp = time;
+                        //Monitor.Exit(myData);
+                }
+                catch (FormatException ex) //FUUUUUCK! Shit happened!
+                {
+                    Console.WriteLine(s);
+                    throw new GPSDataParseException("Format exception!");
+                }
+                return d;
+            }
+            //throw new GPSDataParseException("Format exception!");
+        }
+        
+    }
+}
---
+++ GenericGPSService.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.Ccr.Core;
+using Microsoft.Dss.Core;
+using Microsoft.Dss.Core.Attributes;
+using Microsoft.Dss.ServiceModel.Dssp;
+using Microsoft.Dss.Core.DsspHttp;
+using Microsoft.Dss.ServiceModel.DsspServiceBase;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Xml;
+using W3C.Soap;
+using robomagellan = RoboMagellan;
+
+using submgr = Microsoft.Dss.Services.SubscriptionManager;
+
+namespace RoboMagellan.GenericGPS
+{
+    [DisplayName("GenericGPS")]
+    [Description("The GenericGPS Service")]
+    [Contract(Contract.Identifier)]
+    public class GenericGPSService : DsspServiceBase
+    {
+
+        private static string PORT = "COM1";
+
+        private GenericGPSState _state = new GenericGPSState();
+        private AC12GPS _gps = null;
+        private GpsDataPort _gpsDataPort = new GpsDataPort();
+
+        [ServicePort("/gps", AllowMultipleInstances = false)]
+        private GenericGPSOperations _mainPort = new GenericGPSOperations();
+
+        [Partner("SubMgr", Contract = submgr.Contract.Identifier, CreationPolicy = PartnerCreationPolicy.CreateAlways)]
+        submgr.SubscriptionManagerPort _subMgrPort = new submgr.SubscriptionManagerPort();
+
+        public GenericGPSService(DsspServiceCreationPort creationPort)
+            : base(creationPort)
+        {
+            
+        }
+
+        protected override void Start()
+        {
+            //base.Start();
+            DirectoryInsert();
+
+            _gps = new AC12GPS(PORT, _gpsDataPort);
+
+            SpawnIterator(activateGPSIterator);
+
+            Interleave mainInterleave = ActivateDsspOperationHandlers();
+            mainInterleave.CombineWith(new Interleave(new ExclusiveReceiverGroup(
+                Arbiter.Receive<UTMData>(true, _gpsDataPort, DataReceivedHandler)),
+                new ConcurrentReceiverGroup()));
+
+            //base.Start();
+        }
+
+        public IEnumerator<ITask> activateGPSIterator()
+        {
+            if (!_gps.initializePort())
+            {
+                yield break;
+            }
+            else
+            {
+                _gps.command("$PASHS,PWR,ON");
+                yield return Arbiter.Receive(false, TimeoutPort(10), delegate(DateTime time) { });
+                _gps.command("$PASHQ,PRT");
+                yield return Arbiter.Receive(false, TimeoutPort(10), delegate(DateTime time) { });
+                _gps.command("$PASHQ,RID");
+                yield return Arbiter.Receive(false, TimeoutPort(10), delegate(DateTime time) { });
+                _gps.command("$PASHS,OUT,A,NMEA");
+                /*System.Threading.Thread.Sleep(10);
+                command("$PASHS,NME,GSV,A,ON");*/
+                /*System.Threading.Thread.Sleep(10);
+                command("$PASHS,NME,GGA,A,ON");*/
+                /*System.Threading.Thread.Sleep(10);
+                command("$PASHS,NME,GSA,A,ON");
+                System.Threading.Thread.Sleep(10);
+                command("$PASHS,NME,VTG,A,ON");*/
+                yield return Arbiter.Receive(false, TimeoutPort(10), delegate(DateTime time) { });
+                _gps.command("$PASHS,NME,UTM,A,ON");     //Make GPS send UTM coordinate string
+                yield return Arbiter.Receive(false, TimeoutPort(10), delegate(DateTime time) { });
+                _gps.activateHandler();
+            }
+        }
+
+        [ServiceHandler(ServiceHandlerBehavior.Concurrent)]
+        public IEnumerator<ITask> GetHandler(Get get)
+        {
+            get.ResponsePort.Post(_state);
+            yield break;
+        }
+
+        [ServiceHandler(ServiceHandlerBehavior.Concurrent)]
+        public virtual IEnumerator<ITask> HttpGetHandler(HttpGet httpGet)
+        {
+            httpGet.ResponsePort.Post(new HttpResponseType(_state.Coords));
+            yield break;
+        }
+
+        [ServiceHandler(ServiceHandlerBehavior.Concurrent)]
+        public IEnumerator<ITask> HttpPostHandler(HttpPost httpPost)
+        {
+            yield break;
+        }
+
+        [ServiceHandler(ServiceHandlerBehavior.Concurrent)]
+        public virtual IEnumerator<ITask> SubscribeHandler(Subscribe subscribe)
+        {
+            SubscribeRequestType request = subscribe.Body;
+
+            yield return Arbiter.Choice(
+                SubscribeHelper(_subMgrPort, request, subscribe.ResponsePort),
+                delegate(SuccessResult success)
+                {
+                    SendNotification<UTMNotification>(_subMgrPort, request.Subscriber, _state.Coords);
+                },
+                delegate(Exception e)
+                {
+                    LogError(null, "Subscribe failed", e);
+                }
+            );
+
+            yield break;
+        }
+
+        private void DataReceivedHandler(UTMData d)
+        {
+            _state.Coords = d;
+
+            SendNotification(_subMgrPort, new UTMNotification(d)); 
+        }
+    }
+}
