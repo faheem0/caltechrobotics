0274ac3bd1c250f46efa4fdc857b4174433b8a8c
tonyfwu <tonyfwu@eac4872d-9840-0410-83f7-a96378a11634>
2008-04-01 08:52:43 +0000

    First Import

---
+++ Motors/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RoboMagellan.MotorControl;
+
+namespace RoboMagellan.MotorControl
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string portName = "COM1";
+            motorcontrol motors = new motorcontrol(portName);
+            if(!motors.connect())
+                Console.WriteLine("Couldn't connect to " + portName);
+
+            string cmd;
+            string[] values;
+            while (true)
+            {
+                Console.Write("Command: ");
+                cmd = Console.ReadLine();
+                cmd = cmd.Trim();
+                values = cmd.Split(' ');
+                motors.command(MotorCommands.MOVE, Byte.Parse(values[0]), Byte.Parse(values[1]));
+            }
+        }
+    }
+}
