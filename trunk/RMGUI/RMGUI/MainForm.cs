915c0e46d82ce3853842b99666ee65f9b52772e7
tonyfwu <tonyfwu@eac4872d-9840-0410-83f7-a96378a11634>
2008-05-17 21:04:37 +0000

    Stuff is untested. Need the queue thing.

--- trunk/RMGUI/RMGUI/MainForm.cs
+++ trunk/RMGUI/RMGUI/MainForm.cs
@@ -14,6 +14,8 @@ namespace RMGUI
     {
 
         private ServiceFetcher sf;
+        private TimeSpan time;
+        private long sec;
 
         public MainForm()
         {
@@ -22,18 +24,34 @@ namespace RMGUI
             quitToolStripMenuItem.Click += quitToolStripMenuItem_Click;
             ConsolePipe cp = new ConsolePipe(log);
             Console.SetOut(cp);
+            timer1.Tick += upTimeTextUpdate;
+            sec = 0;
+            time = new TimeSpan();
         }
 
         private void connectToolStripMenuItem_Click(object sender, EventArgs e)
         {
             sf = new ServiceFetcher(new TextBox[4] { SatText, TimeText, EastText, NorthText });
+            UpTimeText.Text = "Up Time: 00:00:00";
+            timer1.Start();
+            connectToolStripMenuItem.Text = "Disconnect";
         }
 
         private void quitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             System.Environment.Exit(0);
         }
-        
+
+        private void upTimeTextUpdate(object sender, EventArgs e)
+        {
+            sec++;
+            time = new TimeSpan(sec * 10000000);
+            UpTimeText.Text = "Up Time: "
+                + String.Format("{0:00}",time.Hours)
+                + ":" + String.Format("{0:00}",time.Minutes)
+                + ":" + String.Format("{0:00}",time.Seconds);
+
+        }
         
 
     }
