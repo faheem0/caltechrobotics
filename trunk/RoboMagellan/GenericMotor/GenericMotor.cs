009df81348ac1e2f86c2941b7043185156e74a91
tonyfwu <tonyfwu@eac4872d-9840-0410-83f7-a96378a11634>
2008-05-21 03:54:30 +0000

    Fixed some bugs

--- trunk/RoboMagellan/GenericMotor/GenericMotor.cs
+++ trunk/RoboMagellan/GenericMotor/GenericMotor.cs
@@ -45,7 +45,7 @@ namespace RoboMagellan.MotorControl
         [ServicePort("/genericmotor", AllowMultipleInstances=false)]
         private GenericMotorOperations _mainPort = new GenericMotorOperations();
 
-        private static string MOTOR_PORT = "COM2";
+        private static string MOTOR_PORT = "COM1";
 
         private MotorControl _motor;
 
