28d6839f106c66c2fac5d44550f7a3c65e6fa25a
customhovercraft <customhovercraft@eac4872d-9840-0410-83f7-a96378a11634>
2008-06-12 13:08:38 +0000

    ROMmed code-starup.hex, DO NOT load startup with the debugger

--- trunk/SamsEECS52/build.bat
+++
@@ -1,5 +0,0 @@
-cls
-@echo off
-echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-IC86 testlcdk.c debug extend mod186 small optimize(0) rom
-call compile main.asm c0smrom.asm display.asm keypad.asm testlcdk.obj
