f51e194e1d034a3c839eec8e3ef6c79cce7d9a34
ksarma <ksarma@eac4872d-9840-0410-83f7-a96378a11634>
2008-06-27 05:12:07 +0000

    Added RoboMagellanCpp project

---
+++ RoboMagellanCpp/MainControl.hh
@@ -0,0 +1,31 @@
+#ifndef __MainControl_hh__
+#define __MainControl_hh__
+
+#include "UTMData.hh"
+#include <list>
+
+
+typedef enum {
+  STATE_STOPPING,
+  STATE_STOPPED,
+  STATE_TURNING,
+  STATE_DRIVING,
+  STATE_ERROR,
+  STATE_STANDBY,
+  STATE_SCANNING
+} MainControlState;
+
+class MainControl {
+public:
+  MainControl() : state(STATE_STANDBY), location(NULL), destination(NULL), destinations(new list<UTMData*>()) {  }
+
+private:
+  MainControlState state;
+  UTMData* location;
+  UTMData* destination;  
+  list<UTMData*> * destinations;
+
+};
+
+
+#endif // __MainControl_hh__
