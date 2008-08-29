85d45097a26ea9e592591983691c8c15d555c976
tonyfwu <tonyfwu@eac4872d-9840-0410-83f7-a96378a11634>
2008-08-29 18:44:56 +0000

    Should Work. Made the charting look better with new library. NOT TESTED

--- java/SerialPlot/src/serialplot/Main.java
+++ java/SerialPlot/src/serialplot/Main.java
@@ -2,96 +2,142 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package serialplot;
+
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+import info.monitorenter.gui.chart.Chart2D;
+import info.monitorenter.gui.chart.ITrace2D;
+import info.monitorenter.gui.chart.traces.Trace2DReplacing;
+import info.monitorenter.gui.chart.traces.Trace2DSimple;
 import javax.swing.*;
-import java.awt.Dimension;
 import java.awt.Color;
 import java.io.*;
-import gnu.io.*;
-import java.util.*;
+import java.util.Scanner;
+
 /**
  *
  * @author tonyfwu
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static final int PLOT_WIDTH = 600;
-    public static final int PLOT_HEIGHT = 600;
-    
-    public static final int NEXT_TIME_BYTE = 254; //defined in protocol w/MCU
-    public static final Color[] pointColor = {Color.RED, Color.GREEN, Color.BLUE,Color.WHITE, Color.ORANGE};
-    
-    public static void main(String[] args) {
-	
+	/**
+	 * @param args the command line arguments
+	 */
+	public static final int PLOT_WIDTH = 600;
+	public static final int PLOT_HEIGHT = 600;
+	public static final int NEXT_TIME_BYTE = 254; //defined in protocol w/MCU
+	public static final Color[] pointColor = {Color.RED, Color.GREEN, Color.BLUE, Color.MAGENTA, Color.ORANGE};
 
-	    JFrame f = new JFrame("SEGWAY!!!");
-	    Plot p = new Plot(PLOT_WIDTH, PLOT_HEIGHT);
-	    p.setScale(1, 3);
-	    f.setPreferredSize(new Dimension(PLOT_WIDTH + 10,PLOT_HEIGHT + 50));
-	    ImagePanel panel = new ImagePanel(p);
-	    f.add(panel);
-	    panel.setVisible(true);
-	    f.setVisible(true);
-	    p.drawAxes(PLOT_HEIGHT/2, 0);
-	    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    f.pack();
+	public static void main(String[] args) {
 
-	    //Plot a function
-	    //y = x^2;
-	    
-	    /*Color c = new Color(0,255,0);
-	    for (int x = -10; x <= 10; x++){
-		p.plotPoint(5*x, x*x, c);
-	    }
-            while(true); */
-	    //Connect to Robot
-	    Color c = new Color(0,255,0);
-	    Robot r = new Robot("COM5");
-	    InputStream in = r.getInputStream();
-            
-	    int byte_read;
-	    int time = 0;
-	    int colorIndex = 0;
-            
-	    while(true){
-		try{
-			byte_read =  in.read();
-                        
-			if (byte_read == -1) continue;
-                        if(byte_read == NEXT_TIME_BYTE) {
-                            time = (time+1) % PLOT_WIDTH;
-                            p.drawVerticalLine(((time+1) % PLOT_WIDTH), Color.CYAN);
-                            p.drawVerticalLine(time, Color.BLACK);
-                            colorIndex = 0;  //reset color index
-                        }
-                        else {                            
-                            byte_read=convertTwosComplement(byte_read);
-                            System.out.println("Time: "+time + "\t"+"Byte read: " + byte_read);		
-                            p.plotPoint(time, byte_read, pointColor[colorIndex]);
-                            if( (colorIndex +1) < pointColor.length)
-                                colorIndex++;
-                        }			
-			panel.repaint();
-		} catch (IOException e){
-			e.printStackTrace();
+
+		JFrame f = new JFrame("SEGWAY!!!");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		Chart2D chart = new Chart2D();
+		final ITrace2D[] trace = new Trace2DSimple[pointColor.length];
+		for (int i = 0; i < pointColor.length; i++) {
+			trace[i] = new Trace2DReplacing();
+			trace[i].setColor(pointColor[i]);
+			chart.addTrace(trace[i]);
 		}
+		f.getContentPane().add(chart);
+		f.setSize(400, 300);
+		//Plot a function
+		//y = x^2;
 
-	    }
+		/*
+		Color c = new Color(0,255,0);
+		for (int x = -10; x <= 10; x++){
+		trace[0].addPoint(5*x, x*x);
+		//		p.plotPoint(5*x, x*x);
+		}
+		f.setVisible(true);
+		while(true); */
 
-    }
-    /**
-     * Basically, this function takes a number and converts it into
-     * 8-bit two's complement.  The number it takes has unknown format,
-     * it could be unsigned or 16-bit signed.  It just works.
-     */
-    public static int convertTwosComplement(int num) {
-        if(num>127)
-            return -1*(256-num);
-        return num;
-    }
+		//Connect to Robot
+		Robot r;
+		Scanner s = new Scanner(System.in);
+		int baud, stop, parity, data;
+		String port;
+		System.out.print("Port: ");
+		port = s.nextLine();
+		port.trim();
+		System.out.print("Baud: ");
+		baud = s.nextInt();
+		System.out.print("Data Bits: ");
+		switch (s.nextInt()) {
+			case 5:
+				data = SerialPort.DATABITS_5;
+				break;
+			case 6:
+				data = SerialPort.DATABITS_6;
+				break;
+			case 7:
+				data = SerialPort.DATABITS_7;
+				break;
+			case 8:
+			default:
+				data = SerialPort.DATABITS_8;
+				break;
+		}
+		System.out.print("Stop Bits: ");
+		float t = s.nextFloat();
+		if (t == 1.5f) {
+			stop = SerialPort.STOPBITS_1_5;
+		} else if (t == 2f) {
+			stop = SerialPort.STOPBITS_2;
+		} else {
+			stop = SerialPort.STOPBITS_1;
+		}
+		parity = SerialPort.PARITY_NONE;
+		r = new Robot(port, baud, data, stop, parity);
+		final InputStream in = r.getInputStream();
 
+		r.setHandler(new SerialPortEventListener() {
+
+			int time = 0;
+			int colorIndex = 0;
+
+			public void serialEvent(SerialPortEvent arg0) {
+				if (arg0.equals(SerialPortEvent.DATA_AVAILABLE)) {
+					try {
+						int byte_read = in.read();
+
+						if (byte_read == -1) {
+							return;
+						}
+						if (byte_read == NEXT_TIME_BYTE) {
+							time = (time + 1) % PLOT_WIDTH;
+							colorIndex = 0;  //reset color index
+						} else {
+							byte_read = convertTwosComplement(byte_read);
+							System.out.println("Time: " + time + "\t" + "Byte read: " + byte_read);
+							trace[colorIndex].addPoint(time, byte_read);
+							if ((colorIndex + 1) < pointColor.length) {
+								colorIndex++;
+							}
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		f.setVisible(true);
+	}
+
+	/**
+	 * Basically, this function takes a number and converts it into
+	 * 8-bit two's complement.  The number it takes has unknown format,
+	 * it could be unsigned or 16-bit signed.  It just works.
+	 */
+	public static int convertTwosComplement(int num) {
+		if (num > 127) {
+			return -1 * (256 - num);
+		}
+		return num;
+	}
 }
