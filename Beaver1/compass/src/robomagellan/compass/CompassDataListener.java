1b7dc6511a29b47ddc4f4eba5b5e5f04f8227430
tonyfwu <tonyfwu@eac4872d-9840-0410-83f7-a96378a11634>
2008-12-06 20:01:52 +0000

    Wrote compass code. Data processing has not been done so it actually only prints out serial port data.

---
+++ Beaver1/compass/src/robomagellan/compass/CompassDataListener.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package robomagellan.compass;
+
+/**
+ *
+ * @author robomagellan
+ */
+public interface CompassDataListener {
+    public void processEvent(CompassPacket c);
+}
