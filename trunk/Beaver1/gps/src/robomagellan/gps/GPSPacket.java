ccd73807664b34fdc349871c7e8fa1c8f65d1c7d
tonyfwu <tonyfwu@eac4872d-9840-0410-83f7-a96378a11634>
2008-12-06 09:46:12 +0000

    First Import. GPS should work. Have not tested outside. Parsers work.

---
+++ trunk/Beaver1/gps/src/robomagellan/gps/GPSPacket.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package robomagellan.gps;
+
+/**
+ *
+ * @author robomagellan
+ */
+public class GPSPacket {
+    public double utmEast = 0;
+    public double utmNorth = 0;
+    public double precision = 0;
+    public double time = 0;
+}
