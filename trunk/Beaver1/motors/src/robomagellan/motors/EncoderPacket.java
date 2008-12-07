7afbafeb27c2cd44b5c8f61f597715f0c34da1e3
tonyfwu <tonyfwu@eac4872d-9840-0410-83f7-a96378a11634>
2008-12-07 01:28:00 +0000

    Added debugging messages to exception handling

---
+++ trunk/Beaver1/motors/src/robomagellan/motors/EncoderPacket.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package robomagellan.motors;
+
+/**
+ *
+ * @author robomagellan
+ */
+public class EncoderPacket {
+
+}
