ef3d478eb9e12324b6aabc1522a4467d90bda0f8
tonyfwu <tonyfwu@eac4872d-9840-0410-83f7-a96378a11634>
2008-12-21 23:30:26 +0000

    Added Javadoc.

--- Beaver1/gps/src/robomagellan/gps/GPSDataListener.java
+++ Beaver1/gps/src/robomagellan/gps/GPSDataListener.java
@@ -2,14 +2,18 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package robomagellan.gps;
 
 /**
- *
+ * An interface to process packets received from the GPS
  * @author robomagellan
  */
 public interface GPSDataListener {
 
-    public void processEvent(GPSPacket g);
+	/**
+	 * The event called by the AC12GPS class to process each packet.
+	 * @param g the packet to be processed
+	 * @see AC12GPS
+	 */
+	public void processEvent(GPSPacket g);
 }
