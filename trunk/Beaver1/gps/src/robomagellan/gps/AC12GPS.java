851837e7997df165ae4fb48f63812a509338c751
tonyfwu <tonyfwu@eac4872d-9840-0410-83f7-a96378a11634>
2008-12-21 23:30:26 +0000

    Added Javadoc.

--- trunk/Beaver1/gps/src/robomagellan/gps/AC12GPS.java
+++ trunk/Beaver1/gps/src/robomagellan/gps/AC12GPS.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package robomagellan.gps;
 
 import gnu.io.SerialPort;
@@ -20,111 +19,163 @@ import java.util.regex.Pattern;
 import robomagellan.helpers.SerialPortFactory;
 
 /**
- *
+ * A class to communicate with the GPS
  * @author robomagellan
  */
 public class AC12GPS {
 
-    public static final int BAUD_RATE = 4800;
-    public static final int DATA_BITS = SerialPort.DATABITS_8;
-    public static final int PARITY = SerialPort.PARITY_NONE;
-    public static final int STOP_BITS = SerialPort.STOPBITS_1;
+	/**
+	 * Baud rate of the GPS
+	 */
+	public static final int BAUD_RATE = 4800;
+	/**
+	 * Number of data bits for the GPS serial port
+	 */
+	public static final int DATA_BITS = SerialPort.DATABITS_8;
+	/**
+	 * Number of parity bits for the GPS serial port
+	 */
+	public static final int PARITY = SerialPort.PARITY_NONE;
+	/**
+	 * Number of stop bits for the GPS serial port
+	 */
+	public static final int STOP_BITS = SerialPort.STOPBITS_1;
+	/**
+	 * Regular expession to detect UTM coordinates. Used for parsing.
+	 */
+	private static final Pattern utm = Pattern.compile("^\\$PASHR,UTM");
+	/**
+	 * Multiplier to calculate the precision of the received data
+	 */
+	private static final double PRECISION_MULT = 3.0;
+	/**
+	 * Buffer size to read into for parsing
+	 */
+	private static final int BUFFER_SIZE = 64;
+	private SerialPort port;
+	private boolean hasListener;
+	private GPSDataListener listener;
+	private InputStream in;
 
-    private static final Pattern utm = Pattern.compile("^\\$PASHR,UTM");
+	/**
+	 * Connects to the GPS and sends the start commands to start acquiring satellites.
+	 * @param portName the port to connect to (ie. "COM1" or "/dev/ttyUSB0")
+	 */
+	public AC12GPS(String portName) {
+		hasListener = false;
+		port = SerialPortFactory.openPort(portName, BAUD_RATE, DATA_BITS, STOP_BITS, PARITY);
+		try {
+			in = port.getInputStream();
+			OutputStream out = port.getOutputStream();
+			String cmd;
+			cmd = "$PASHS,PWR,ON\r\n";
+			cmd = cmd + "$PASHQ,PRT\r\n";
+			cmd = cmd + "$PASHQ,RID\r\n";
+			cmd = cmd + "$PASHS,OUT,A,NMEA\r\n";
+			cmd = cmd + "$PASHS,NME,UTM,A,ON\r\n";
+			out.write(cmd.getBytes("ASCII"));
+		} catch (IOException ex) {
+			Logger.getLogger(AC12GPS.class.getName()).log(Level.SEVERE, "COuld not get OutputStream or InputStream or could not write to OutputStream", ex);
+		}
 
-    private static final double PRECISION_MULT = 3.0;
-    private static final int BUFFER_SIZE = 64;
+	}
 
-    private SerialPort port;
-    private boolean hasListener;
-    private GPSDataListener listener;
+	/**
+	 * Starts reading and parsing data from the serial port. The data is passed to the GPSDataListener for processing.
+	 * @param g the processEvent method of this object is called to process the GPS coordinates.
+	 * @throws java.util.TooManyListenersException
+	 */
+	public synchronized void addGPSDataListener(GPSDataListener g) throws TooManyListenersException {
+		if (hasListener) {
+			throw new TooManyListenersException("There's already a listener on " + port.getName());
+		}
+		listener = g;
+		port.notifyOnDataAvailable(true);
+		port.addEventListener(new SerialPortEventListener() {
 
-    private InputStream in;
+			String str = "";
+			byte[] buffer = new byte[BUFFER_SIZE];
+			int bytesRead;
+			String[] strs;
+			String[] params;
+			Matcher m;
 
-    public AC12GPS(String portName){
-        hasListener = false;
-        port = SerialPortFactory.openPort(portName, BAUD_RATE, DATA_BITS, STOP_BITS, PARITY);
-        try {
-            in = port.getInputStream();
-            OutputStream out = port.getOutputStream();
-            String cmd;
-            cmd = "$PASHS,PWR,ON\r\n";
-            cmd = cmd + "$PASHQ,PRT\r\n";
-            cmd = cmd + "$PASHQ,RID\r\n";
-            cmd = cmd + "$PASHS,OUT,A,NMEA\r\n";
-            cmd = cmd + "$PASHS,NME,UTM,A,ON\r\n";
-            out.write(cmd.getBytes("ASCII"));
-        } catch (IOException ex) {
-            Logger.getLogger(AC12GPS.class.getName()).log(Level.SEVERE, "COuld not get OutputStream or InputStream or could not write to OutputStream", ex);
-        }
+			public void serialEvent(SerialPortEvent arg0) {
+				if (arg0.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+					try {
+						bytesRead = in.read(buffer);
+						str = str.concat(new String(buffer, 0, bytesRead, "ASCII"));
+						//str = new String(buffer, 0, bytesRead, "ASCII");
+						strs = str.split("\r\n");
+						str = strs[strs.length - 1];
+						for (int i = 0; i < strs.length - 1; i++) {
+							m = utm.matcher(strs[i]);
+							if (m.find()) {
+								GPSPacket packet = getPacket(strs[i]);
+								listener.processEvent(packet);
+							}
 
-    }
-    public synchronized void addGPSDataListener(GPSDataListener g) throws TooManyListenersException{
-        if(hasListener) throw new TooManyListenersException("There's already a listener on " + port.getName());
-        listener = g;
-        port.notifyOnDataAvailable(true);
-        port.addEventListener(new SerialPortEventListener(){
-            String str = "";
-            byte[] buffer = new byte[BUFFER_SIZE];
-            int bytesRead;
-            String[] strs;
-            String[] params;
-            Matcher m;
+						}
+						System.out.print(str);
 
-            public void serialEvent(SerialPortEvent arg0) {
-                if (arg0.getEventType() == SerialPortEvent.DATA_AVAILABLE){
-                    try {
-                        bytesRead = in.read(buffer);
-                        str = str.concat(new String(buffer, 0, bytesRead, "ASCII"));
-                        //str = new String(buffer, 0, bytesRead, "ASCII");
-                        strs = str.split("\r\n");
-                        str = strs[strs.length-1];
-                        for (int i = 0; i < strs.length -1; i++){
-                            m = utm.matcher(strs[i]);
-                            if (m.find()){    
-                                GPSPacket packet = getPacket(strs[i]);
-                                listener.processEvent(packet);
-                            }
+					//listener.processEvent(null);
+					} catch (IOException ex) {
+						Logger.getLogger(AC12GPS.class.getName()).log(Level.SEVERE, "Could not read from InputStream", ex);
+					}
+				}
+			}
+		});
 
-                        }
-                        System.out.print(str);
+		hasListener = true;
+	}
 
-                        //listener.processEvent(null);
-                    } catch (IOException ex) {
-                        Logger.getLogger(AC12GPS.class.getName()).log(Level.SEVERE, "Could not read from InputStream", ex);
-                    }
-                }
-            }
-        });
+	/**
+	 * Stops reading from the GPS and shuts down the port. This object cannot reconnect to the port after so
+	 * another instance of this class needs to be instantiated.
+	 */
+	public void stop() {
+		port.close();
+	}
 
-        hasListener = true;
-    }
-    public void stop(){
-        port.close();
-    }
+	/**
+	 * Changes the timestamp from the GPS to a number in seconds
+	 * @param timestamp the timestamp number from the GPS
+	 * @return a timestamp number in seconds
+	 */
+	public static double processTime(double timestamp) {
+		double time;
+		timestamp /= 10000;
+		time = Math.floor(timestamp) * 3600;
+		timestamp = 100 * timestamp - 100 * Math.floor(timestamp);
+		time += Math.floor(timestamp) * 60;
+		timestamp = 100 * timestamp - 100 * Math.floor(timestamp);
+		time += timestamp;
+		return time;
+	}
 
-    public static double processTime(double timestamp) {
-        double time;
-        timestamp /= 10000;
-        time = Math.floor(timestamp) * 3600;
-        timestamp = 100*timestamp - 100*Math.floor(timestamp);
-        time += Math.floor(timestamp) * 60;
-        timestamp = 100*timestamp - 100*Math.floor(timestamp);
-        time += timestamp;
-        return time;
-    }
-
-    public static GPSPacket getPacket(String str){
-        GPSPacket packet = new GPSPacket();
-        String[] params = str.split(",");
-        if (!params[4].equals("")) packet.utmEast = Double.parseDouble(params[4]);
-        if (!params[5].equals(""))packet.utmNorth = Double.parseDouble(params[5]);
-        if (!params[8].equals(""))packet.precision = Double.parseDouble(params[8]) * PRECISION_MULT;
-
-        double timestamp = 0;
-        if (!params[2].equals("")) timestamp = Double.parseDouble(params[2]);
-        packet.time = processTime(timestamp);
-        return packet;
-    }
-
+	/**
+	 * Used to create a GPSPacket from the NMEA UTM string from the GPS
+	 * @param str the string from the GPS
+	 * @return a new GPSPacket
+	 * @see GPSPacket
+	 */
+	public static GPSPacket getPacket(String str) {
+		GPSPacket packet = new GPSPacket();
+		String[] params = str.split(",");
+		if (!params[4].equals("")) {
+			packet.utmEast = Double.parseDouble(params[4]);
+		}
+		if (!params[5].equals("")) {
+			packet.utmNorth = Double.parseDouble(params[5]);
+		}
+		if (!params[8].equals("")) {
+			packet.precision = Double.parseDouble(params[8]) * PRECISION_MULT;
+		}
+		double timestamp = 0;
+		if (!params[2].equals("")) {
+			timestamp = Double.parseDouble(params[2]);
+		}
+		packet.time = processTime(timestamp);
+		return packet;
+	}
 }
--- trunk/Beaver1/gps/src/robomagellan/gps/GPSPacket.java
+++ trunk/Beaver1/gps/src/robomagellan/gps/GPSPacket.java
@@ -2,16 +2,28 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package robomagellan.gps;
 
 /**
- *
+ * A GPS Packet
  * @author robomagellan
  */
 public class GPSPacket {
-    public double utmEast = 0;
-    public double utmNorth = 0;
-    public double precision = 0;
-    public double time = 0;
+
+	/**
+	 * UTM East coordinates in meters
+	 */
+	public double utmEast = 0;
+	/**
+	 * UTM North coordinates in meters
+	 */
+	public double utmNorth = 0;
+	/**
+	 * Precision of the coordinates in +- meters
+	 */
+	public double precision = 0;
+	/**
+	 * Timestamp in seconds that in which a this packet was received.
+	 */
+	public double time = 0;
 }
