92da651eaba434c8d0a447ae3fa718bb1f3de920
tonyfwu <tonyfwu@eac4872d-9840-0410-83f7-a96378a11634>
2008-12-21 16:49:40 +0000

    Added Javadoc

--- trunk/Beaver1/compass/src/robomagellan/compass/CompassPacket.java
+++ trunk/Beaver1/compass/src/robomagellan/compass/CompassPacket.java
@@ -6,9 +6,13 @@
 package robomagellan.compass;
 
 /**
- *
+ * The compass reading will be wrapped in this class.
  * @author robomagellan
  */
 public class CompassPacket {
+	/**
+	 * The heading read by the compass ranging from 0 to 359 degrees. 0 is north.
+	 * Ignore the default value.
+	 */
     public int heading = 0xFF;
 }
