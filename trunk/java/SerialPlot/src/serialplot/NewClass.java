52f8335f2d65f0bcb7f32397f8e0e0ac984cb2ab
customhovercraft <customhovercraft@eac4872d-9840-0410-83f7-a96378a11634>
2009-05-17 23:30:22 +0000

    old code, tested on 5/17/09 and it works, although may need to be recompiled

---
+++ trunk/java/SerialPlot/src/serialplot/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package serialplot;
+
+/**
+ *
+ * @author Sam
+ */
+public class NewClass {
+
+}
